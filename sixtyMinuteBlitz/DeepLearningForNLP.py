7132f27071dd79fc3d2f96a0c3360e232a749372
samirrajpurkar <samir.rajpurkar@gmail.com>
2017-07-24 21:01:11 +0400

    torch concatenation

--- sixtyMinuteBlitz/DeepLearningForNLP.py
+++ sixtyMinuteBlitz/DeepLearningForNLP.py
@@ -49,3 +49,24 @@ y = torch.Tensor([4., 5., 6.])
 z = x + y
 print(z)
 
+# One helpful operation that we will make
+# use of later is concatenation
+
+# By default, it concatenates along
+# the first axis (concatenates rows)
+x_1 = torch.randn(2, 5)
+print('x_1', x_1)
+y_1 = torch.randn(3, 5)
+print('y_1', y_1)
+z_1 = torch.cat([x_1, y_1])
+print(z_1)
+
+# Concatenates columns:
+x_2 = torch.randn(2, 3)
+print('x_2', x_2)
+y_2 = torch.randn(2, 5)
+print('y_2', y_2)
+z_2 = torch.cat([x_2, y_2], 1)
+print('z_2', z_2)
+
+
