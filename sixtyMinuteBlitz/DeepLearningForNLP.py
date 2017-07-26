ccc1a6a4ba12ac9b764cc599bfcc81cb7b34c197
samirrajpurkar <samir.rajpurkar@gmail.com>
2017-07-26 21:44:54 +0400

    deep learning building blocks

--- sixtyMinuteBlitz/DeepLearningForNLP.py
+++ sixtyMinuteBlitz/DeepLearningForNLP.py
@@ -80,5 +80,75 @@ print('same as above', x.view(2, -1))
 # Computation Graphs
 # and Automatic Differentiation
 
+# Variables wrap tensor objects
+x = autograd.Variable(torch.Tensor([1., 2., 3.]), requires_grad=True)
+# you can access the data with the .data attribute
+print('data in x Variable', x.data)
+
+y = autograd.Variable(torch.Tensor([4., 5., 6.]), requires_grad=True)
+print('data in y Variable', y.data)
+
+z = x + y
+
+print ('z = x+y', z.data)
+
+print('But z knows something extra', z.creator)
+
+s = z.sum()
+print('s = z.sum()', s)
+print('s.creator', s.creator)
+
+print('calling backward() on any variable will run backprop, starting from it', s.backward())
+print('ds / dx', x.grad)
+
+# Understanding what is going on
+# in the block below is crucial for being
+# a successful programmer in deep learning
+
+x = torch.randn((2, 2))
+y = torch.randn((2, 2))
+z = x + y
+
+print('x', x)
+print('y', y)
+print('z', z)
+
+var_x = autograd.Variable(x)
+var_y = autograd.Variable(y)
+var_z = var_x + var_y
+
+print('var_z.creator', var_z.creator)
+
+var_z_data = var_z.data
+new_var_z = autograd.Variable(var_z_data)
+print('new_var_z', new_var_z.creator)
+
+
+print('-----------------------------------')
+print('---Deep Learning Building Blocks---')
+lin = nn.Linear(5, 3)
+# maps from r^5 to r^3
+# parameters A, b
+# data is 2 x 5
+# A maps from 5 to 3
+print('lin.weight.data', lin.weight.data)
+print('lin.bias.data', lin.bias.data)
+x_v = autograd.Variable(torch.randn(2, 5))
+print('x_v.data', x_v.data)
+print('lin(data)', lin(x_v))
+
+# Note that non-linearites typically
+# don't have paramaters like affine maps do.
+# That is, they don't have weights that
+# are updated during training.
+
+print('relu(x_v)', F.relu(x_v))
+
+# Softmax is also in torch.functional
+data_v = autograd.Variable(torch.randn(5))
+print('data_v', data_v)
+print('F.softmax(data_v)', F.softmax(data_v))
+print('F.softmax(data_v).sum() = 1', F.softmax(data_v).sum())
+print('There is also log_softmax(data)', F.log_softmax(data_v))
 
 
