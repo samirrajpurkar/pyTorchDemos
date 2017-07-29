cd94b0bca9a8f347a3a4d3361aeffaab8814a40c
samirrajpurkar <samir.rajpurkar@gmail.com>
2017-07-29 10:58:09 +0400

    Linear Model fine tuning

--- sixtyMinuteBlitz/LogisticRegressionBagOfWords.py
+++ sixtyMinuteBlitz/LogisticRegressionBagOfWords.py
@@ -12,8 +12,10 @@ data = [
     ("No it is not a good idea to get lost at sea".split(), "ENGLISH")
 ]
 
-test_data = [("Yo creo que si".split(), "SPANISH"),
-             ("it is lost on me".split(), "ENGLISH")]
+test_data = [
+    ("Yo creo que si".split(), "SPANISH"),
+    ("it is lost on me".split(), "ENGLISH")
+]
 
 word_to_ix = {}
 label_to_ix = {"SPANISH": 0, "ENGLISH": 1}
@@ -23,16 +25,14 @@ for sentence, _ in data + test_data:
         if word not in word_to_ix:
             word_to_ix[word] = len(word_to_ix)
 
-# print('len(word_to_ix)', len(word_to_ix))
-
 VOCAB_SIZE = len(word_to_ix)
 NUM_LABELS = 2
 
 
 class BoWClassifier(nn.Module):  # inheriting from nn.Module!
 
-    def __init__(self, num_labels, vocab_size):  # calls the init function of nn.Module
-        super(BoWClassifier, self).__init__()
+    def __init__(self, num_labels, vocab_size):
+        super(BoWClassifier, self).__init__()  # calls the init function of nn.Module
         # In this case, we need A and b parameters
         # the parameters of the affine mapping.
         # Torch defines nn.Linear()
@@ -63,6 +63,9 @@ def make_target(p_label, p_label_to_ix):
 
 def main():
     model = BoWClassifier(NUM_LABELS, VOCAB_SIZE)
+
+    # for param in model.parameters():
+    #     print(type(param.data), param.size())
     # the model knows it parameters.
     # the first output is A, the second is b
 
@@ -74,19 +77,19 @@ def main():
 
     # run on test data before we train,
     # just to see a before-and-after
-
-    for instance, label in test_data:
-        bow_vector = autograd.Variable(make_bow_vector(instance, word_to_ix))
-        log_probs = model(bow_vector)
-        # print ("Log P test_data before train", log_probs, label)
-
-    # Print the matrix column corresponding to "creo"
-    print("Pre - creo", next(model.parameters())[:, word_to_ix["creo"]])
+    #
+    # for instance, label in test_data:
+    #     bow_vector = autograd.Variable(make_bow_vector(instance, word_to_ix))
+    #     log_probs = model(bow_vector)
+    #     # print ("Log P test_data before train", log_probs, label)
+    #
+    # # Print the matrix column corresponding to "creo"
+    # print("pre - creo", next(model.parameters())[:, word_to_ix["creo"]])
 
     loss_function = nn.NLLLoss()
     optimizer = optim.SGD(model.parameters(), lr=0.1)
 
-    for epoch in range(100):
+    for epoch in range(1):
         for instance, label in data:
             # Step 1: remmber that pytorch accumulates gradients
             # we need to clear them out before each instance
@@ -99,11 +102,15 @@ def main():
             # of log probabilities is the log probability corresponding
             # to SPANISH
             bow_vector = autograd.Variable(make_bow_vector(instance, word_to_ix))
+            print('bow_vector', bow_vector)
+
             target = autograd.Variable(make_target(label, label_to_ix))
+            print('target', target, label)
 
             # Step 3:
             # run our forward pass
             log_probs = model(bow_vector)
+            print('Log P', log_probs)
 
             # Step 4:
             # Compute the loss, gradients, and update the paramaters
@@ -112,15 +119,31 @@ def main():
             loss.backward()
             optimizer.step()
 
-    for instance, label in test_data:
-        bow_vector = autograd.Variable(make_bow_vector(instance, word_to_ix))
-        log_probs = model(bow_vector)
-        print("Log P test_data after train", log_probs, label)
+    # for instance, label in test_data:
+    #     bow_vector = autograd.Variable(make_bow_vector(instance, word_to_ix))
+    #     log_probs = model(bow_vector)
+    #     print("Log P test_data after train", log_probs, label)
 
     # Index corresponding to Spanish goes up,
     # English goes down
-    print("Post - creo", next(model.parameters())[:, word_to_ix["creo"]])
+    # print("Post - creo", next(model.parameters())[:, word_to_ix["creo"]])
+
+
+def test():
+    # vec = torch.zeros(len(word_to_ix))
+    # print(vec)
+    # print(vec.size())
+    # print(vec.view(1, -1).size())
+
+    # m = nn.Linear(2, 3)
+    # input = autograd.Variable(torch.randn(5, 2))
+    # print(input.size())
+    # output = m(input)
+    # print (output.size())
+    # for param in m.parameters():
+    #     print (param.data, param.size)
+    pass
 
 
 if __name__ == '__main__':
-    main()
+   main()
